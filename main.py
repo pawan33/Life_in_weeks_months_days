6d5a527b84f93029c1c2b63aec5cd37ff608c4fe
Pawan Deshmane <pawandshmn@gmail.com>
2022-09-08 21:23:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+years_rem= 90-int(age)
+weeks_rem = years_rem*52
+months_rem = years_rem * 12
+days_rem = weeks_rem * 7
+
+print(f"You have {days_rem} days, {weeks_rem} weeks and {months_rem} months remaining in your life")
+
+
+
+
+
+
+
